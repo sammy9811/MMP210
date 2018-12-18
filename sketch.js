96de293ef775186772665fb11e2eb8247500eecb
sammy9811 <42903667+sammy9811@users.noreply.github.com>
2018-12-18 14:21:17 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,146 @@
-/*
-Self Portrait
-By Samuel Delacruz
-*/
+var serial;
+var portName = "/dev/cu.usbmodem14131";
+var sensorValue;
+var cloudOne = 200;
+var cloudSpeed = 2;
+
+var runwayX = 2200;
+var runwayY = 720;
+var runwayW = 1000;
+var runwayH = 400;
+
+var planeSound;
+ 
+function preload() {
+    dog = loadImage("airplane.png");
+    port = loadImage("airport.png");
+    
+    planeSound = loadSound('Airplane-landing.wav');
+     
+}
+
+
 function setup() {
-	createCanvas(1890, 980);
+    createCanvas(2130, 1050);
+    
+    planeSound.play();
+
+    serial = new p5.SerialPort();
+    serial.on('connected', serverConnected);
+    serial.on('open', portOpen);
+    serial.on('data', serialEvent);
+    serial.on('error', serialError);
+    serial.on('close', portClose);
+
+    serial.open("/dev/cu.usbmodem14131");
+}
+
+function serverConnected() {
+    console.log('connected to server.');
+}
+
+function portOpen() {
+    console.log('the serial port opened.')
+}
+
+function portClose() {
+    console.log('The serial port closed.');
+}
+
+function serialError() {
+    console.log("error");
+}
+
+function serialEvent() {
+    var currentString = serial.readLine();
+    trim(currentString);
+    if (!currentString) {
+        return;
+    }
+    sensorValue = currentString;
 }
 
 function draw() {
-	background("brown");
-	noStroke();
-     
-    // defaults
-	stroke('black');
-	fill('white');
-	
-    // face
-	fill("#fdbc89");
-	ellipse(900, 500, 500, 650);
-	
-    // hair
-  fill('black');
-  arc(width/2.1, height/2.3, 466, 500, PI+.3, -.4, OPEN);
+	var c = map(sensorValue, 0, 800, 0, 180);
 
-    // eye brows
-   stroke('black');
-  strokeWeight(10);
-  noFill();
-  arc(990, 410, 80, 80, PI+.9, -.9);
-  arc(820, 410, 80, 80, PI+.9, -.9);
-   
-    //ears
-    fill("#fdbc89")
-  ellipse(640,492,10,100);
-  ellipse(1160,492,10,100);
-	
-    // eyes
-    strokeWeight(5);
-	fill("white");
-	ellipse(820, 440, 50, 100);
-	ellipse(990, 440, 50, 100);
-	
-	// pupil
-	fill("black");
-	ellipse(820, 440, 50, 50);
-	ellipse(990, 440, 50, 50);
+	// sky
+    fill('');
+	background(c, c, c + 55);
     
-    //nose
-    beginShape(TRIANGLES);
-    vertex(890, 600);
-    vertex(900, 500);
-    vertex(910, 600);
-    endShape();
+
+	// sun
+	var y = map(sensorValue, 100, 1023, height,0);
+    
+//    var runwayW = map(sensorValue, 100, 1023, height,0);
+        
+    var faceY = mouseY;
+    var faceX = mouseX;
+
+	// ground
+	fill(c, c + 0, c);
+	//rect(0, height * 0.75, c, c, c, y, width, height * 0.25);
+    fill('grey');
+    
+
+    
+    image(port, runwayX, runwayY, runwayW, runwayH);
+    
+    runwayX--;
+    
+    if(runwayX < 1200) {
+        runwayX = 1200;
+    }
+    
+    
+    
+    
+    if (mouseX > runwayX && mouseX < runwayX + runwayW && y > runwayY && y < runwayY + runwayH) {
+        
+        noLoop();
+        fill('red')
+        textSize(45);
+        text("YOU HAVE SUCCESSFULLY LANDED THE PLANE! PRESS SPACE TO TRY AGAIN.", 200, 400);
+    }
+    
+    
+
+
+	fill('GOLD');
+	noStroke();
+    image(dog, mouseX , y + 50, 500);
+	ellipse(width/1.1, c, c, c, y, 400);
+    
+    
   
-    //mouth
-  fill('white')
-  rect(850,620,150,150);
+  // cloud
+    fill("white");
+      ellipse(cloudOne, 100, 100, 50)
+    ellipse(cloudOne - 100, 200, 100, 50)
+    ellipse(cloudOne + 200, 150, 100, 50)
+    ellipse(cloudOne + 300, 100, 100, 50)
+    
+    cloudOne = cloudOne + cloudSpeed;
+
+
+    if (cloudOne > 2100) {
+        cloudOne = 0;
+    }
+    
+  
+    }
+function button(x, y, s) {
+	var d = dist(mouseX, mouseY, x, y);
+	if (d < s / 2) {
+		fill('gold');
+	} else {
+		fill('plum');
+	}
+	ellipse(x, y, s);
+}
+
+
+function keyPressed() {
+    if(keyCode == 32) {
+        location.reload();
+    }
 }
